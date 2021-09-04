7bd7036ce5702137e206d18c2e4954c061227e5a
mayurkalekar087 <mayurkalekar087@gmail.com>
2021-09-04 12:04:40 +0530

    feat:Ability To Create Table For AddressBook DB

--- AddressBookSQL.sql
+++ AddressBookSQL.sql
@@ -1,3 +1,17 @@
 create database AddressbookDB
 use AddressbookDB
 select DB_NAME();
+
+create table AddressBook
+(
+ ID int identity(1,1) primary key,
+ firstname varchar(100) not null,
+ lastname varchar(100) not null,
+ address varchar(250) not null,
+ city varchar(100) not null,
+ state varchar(100) not null,
+ zip varchar(6) not null,
+ phonenumber varchar(12) not null,
+ email varchar(100) not null
+);
+select * from AddressBook;
